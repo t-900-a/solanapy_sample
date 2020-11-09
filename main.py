b614b4fb878049a745162ed769bcc55a9725ab35
t-900-a <61916790+t-900-a@users.noreply.github.com>
2020-11-08 22:51:29 -0500

    looping through all transactions within a given account

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ from solana.system_program import decode_transfer
 from solana.system_program import TransferParams
 from solana.transaction import Transaction, TransactionInstruction
 import base64
+# solana_client = Client("https://api.mainnet-beta.solana.com")
 solana_client = Client("https://devnet.solana.com")
-tx_sig = ""
-tx_result = solana_client.get_confirmed_transaction(tx_sig=tx_sig, encoding="base64")
+address = ""
+transactions = solana_client.get_confirmed_signature_for_address2(address)["result"]
+for tx in transactions:
+	tx_result = solana_client.get_confirmed_transaction(tx_sig=tx["signature"], encoding="base64")
 
-raw_tx_str = tx_result['result']['transaction'][0]
+	raw_tx_str = tx_result['result']['transaction'][0]
 
-raw_tx_base64_bytes = raw_tx_str.encode('ascii')
-raw_tx_bytes = base64.b64decode(raw_tx_base64_bytes)
+	raw_tx_base64_bytes = raw_tx_str.encode('ascii')
+	raw_tx_bytes = base64.b64decode(raw_tx_base64_bytes)
 
-tx: Transaction = Transaction.deserialize(raw_tx_bytes)
-tx_instruction: TransactionInstruction = tx.instructions.pop()
-transfer_params: TransferParams = decode_transfer(tx_instruction)
+	tx: Transaction = Transaction.deserialize(raw_tx_bytes)
+	tx_instruction: TransactionInstruction = tx.instructions.pop()
+	print(tx_instruction)
+	if tx_instruction.program_id.__str__() == "Stake11111111111111111111111111111111111111":
+		continue
+	transfer_params: TransferParams = decode_transfer(tx_instruction)
 
-print(transfer_params.to_pubkey) # to
-print(transfer_params.from_pubkey) # from
-print(tranfer_params.lamports) # amount
+	print(transfer_params.to_pubkey) # to
+	print(transfer_params.from_pubkey) # from
+	print(transfer_params.lamports * .000000001) # amount
